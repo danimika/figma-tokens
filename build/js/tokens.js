ace25e61530263691d0daf7591b5337e4125eabb
Dani Mika <dani.mika@gmail.com>
2024-02-13 09:26:34 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 13 Feb 2024 12:11:59 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8257e6",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionPurple500": "#8257e6ff",
+  "VariableCollectionGray200": "#c1c1c4ff",
+  "VariableCollectionGray500": "#50505cff",
+  "VariableCollectionGray800": "#1c1c1fff"
+};
